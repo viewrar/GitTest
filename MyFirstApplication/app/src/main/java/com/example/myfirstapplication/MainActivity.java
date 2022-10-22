99e18f263dd331edf069809fce7a803853345480
Sonder <mralexey1@mail.ru>
2022-10-22 16:49:35 +0300

    Initial commit

---
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    TextView counter;
+    TextView addition;
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        counter = findViewById(R.id.counter);
+        addition = findViewById(R.id.counterAddition);
+        counter.setText(String.valueOf(count));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                counter.setText(String.valueOf(count));
+                if (count == 50) {
+                    Toast.makeText(MainActivity.this, "50 clicks already, keep going", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
